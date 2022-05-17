a205b1a06bb71888de5215814f2181f3dc2ee7c2
uoyhed <uoyhed@gmail.com>
2022-05-17 15:07:01 +0900

    complete commit v1.0

--- index.py
+++ index.py
@@ -1,13 +1,39 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# index
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
+# 선택지 고르는 화면 (시작하기 버튼 클릭)
 @app.route('/first_choice')
 def first_choice():
-    return render_template('first_choice.html')
+    return render_template('first.html')
+
+# 로딩 화면
+@app.route('/loading/<int:value>')
+def loading(value):
+    return render_template('loading.html', value = value)
+
+# 결과 화면
+@app.route('/result/<int:value>')
+def result(value):
+    if value == 1:
+        return render_template('assassin.html')
+    elif value == 2:
+        return render_template('range.html')
+    elif value == 3:
+        return render_template('supporter.html')
+    elif value == 4:
+        return render_template('warrior.html')
+    elif value == 5:
+        return render_template('tanker.html')
+    elif value == 6:
+        return render_template('amumu.html')
+    else:
+        return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/common.js
+++ static/js/common.js
@@ -2,7 +2,22 @@
 // 시작하기 버튼 클릭하면 다음 화면으로 이동
 var start_button = document.getElementById('start_button');
         
-function startClick(){
-    // 시작화면 버튼 누르면 화면 이동
+// 시작하기 버튼 누르면 화면 이동
+function start(){
     window.location.href = '/first_choice';
+}
+
+// 처음으로 이동
+function back(){
+    window.location.href = '/index';
+}
+
+// 선택지 클릭시 대기화면으로 이동
+function loading(value){
+    window.location.href = '/loading/' + value;
+}
+
+// 마지막 결과화면으로 이동
+function result(value){
+    window.location.href = '/result/' + value;
 }
